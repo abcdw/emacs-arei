6d775b57d99b24a5094ca714d7d81d2d2d8e594f
Andrew Tropin <andrew@trop.in>
2023-12-04 20:01:09 +0300

    Remove reference to non-existing function

--- arei.el
+++ arei.el
@@ -61,8 +61,7 @@ preciese way."
 
 (defun arei--get-command-keybindings (command)
   "Return key bindings for COMMAND as a comma-separated string."
-  (let ((keys (mapcar 'arei--key-description
-                      (where-is-internal command nil nil t))))
+  (let ((keys (mapcar 'key-description (where-is-internal command nil nil t))))
     (if keys
         (mapconcat 'identity keys ", ")
       "M-x ... RET")))
