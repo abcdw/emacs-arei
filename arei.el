33f4f868d3fd0bc2eeac17559dc5a35b742f2696
Nikita Domnitskii <nikita@domnitskii.me>
2024-04-09 18:59:47 +0600

    Add eldoc support

--- arei.el
+++ arei.el
@@ -647,6 +647,122 @@ with prefix argument."
 
 
 ;;;
+;;; Eldoc
+;;;
+
+(defvar arei--eldoc-last-sym nil)
+
+(defun arei-eldoc-arglist (callback)
+  "Echo procedure arguments at point by calling CALLBACK.
+Intended for ‘eldoc-documentation-functions’ (which see)."
+  (pcase (arei--eldoc-thing)
+    (`(,sym . ,pos)
+     (if (string= sym (car arei--eldoc-last-sym))
+         (arei--eldoc-display-arglists
+          sym pos (cdr arei--eldoc-last-sym) callback)
+       (let ((req (arei-nrepl-dict
+                   "op" "lookup"
+                   "sym" sym)))
+         (when-let* ((module (arei--get-module)))
+           (arei-nrepl-dict-put req "ns" module))
+         (arei-send-request
+          req
+          (arei-connection-buffer)
+          (arei--eldoc-callback sym pos callback)
+          t)
+         'wait-for-response)))))
+
+(defun arei--eldoc-callback (sym pos callback)
+  (lambda (response)
+    (when-let* ((info (arei-nrepl-dict-get response "info")))
+      (setq arei--eldoc-last-sym (cons sym info))
+      (arei--eldoc-display-arglists sym pos info callback))))
+
+(defun arei--eldoc-display-arglists (sym pos info callback)
+  (pcase info
+    ((map module arglists)
+     (funcall callback
+              (mapconcat
+               (lambda (arglist)
+                 (arei--eldoc-format-arglist arglist pos))
+               arglists
+               " | ")
+              :thing (format "%s %s" module sym)
+              :face 'font-lock-function-name-face))))
+
+(defun arei--eldoc-format-arglist (arglist pos)
+  (pcase arglist
+    ((map required optional keyword allow-other-keys? rest)
+     (let* ((arglist `(,@(when required required)
+                       ,@(when optional (cons "#:optional" optional))
+                       ,@(when keyword (cons "#:key" keyword))
+                       ,(when allow-other-keys? "#:allow-other-keys")
+                       ,@(when rest (list "." rest))))
+            (arglist (seq-remove #'null arglist))
+            (highlighted-arglist (arei--eldoc-highlight-args arglist pos)))
+       (concat "(" (string-join highlighted-arglist " ") ")")))))
+
+(defun arei--eldoc-highlight-args (arglist pos)
+  (let ((rest-pos (seq-position arglist ".")))
+    (thread-first
+      (seq-reduce (lambda (acc arg)
+                    (pcase-let (((map :idx :args) acc))
+                      (cond
+                       ((null arg) acc)
+                       ((or
+                         (string-prefix-p "#:" arg)
+                         (string= arg "."))
+                        (map-put! acc :args (cons arg args))
+                        acc)
+                       ((or (and (integerp pos)
+                                 (or (= (1+ idx) pos)
+                                     (and rest-pos
+                                          (> (1+ idx) rest-pos)
+                                          (> pos rest-pos))))
+                            (and (stringp pos) (string= arg pos)))
+                        (let ((arg (propertize arg 'face 'eldoc-highlight-function-argument)))
+                          (map-put! acc :args (cons arg args))
+                          (map-put! acc :idx (1+ idx))
+                          acc))
+                       (t
+                        (map-put! acc :args (cons arg args))
+                        (map-put! acc :idx (1+ idx))
+                        acc))))
+                  arglist
+                  (list :idx 0 :args nil))
+      (map-elt :args)
+      (seq-reverse))))
+
+(defun arei--eldoc-thing ()
+  (save-excursion
+    (when-let* ((pos (arei--beginning-of-sexp))
+                (thing (thing-at-point 'symbol t)))
+      (cons thing pos))))
+
+(defun arei--beginning-of-sexp ()
+  (let ((parse-sexp-ignore-comments t))
+    (named-let lp ((key (when-let* ((key (thing-at-point 'sexp t))
+                                    ((string-prefix-p "#:" key)))
+                          (substring key 2)))
+                   (pos (or (ignore-errors
+                              (let ((p (point)))
+                                (forward-sexp -1)
+                                (forward-sexp 1)
+                                (when (< (point) p) 1)))
+                            0)))
+      (let ((p (point)))
+        (ignore-errors (forward-sexp -1))
+        (cond
+         ((and (null key)
+               (string-prefix-p "#:" (thing-at-point 'sexp t)))
+          (lp (substring (thing-at-point 'sexp t) 2) pos))
+         ((< (point) p)
+          (lp key (1+ pos)))
+         (t
+          (or key (max 0 (1- pos)))))))))
+
+
+;;;
 ;;; arei-mode
 ;;;
 
@@ -686,7 +802,7 @@ variable to nil to disable the mode line entirely.")
   (if arei-mode
       (progn
         (setq sesman-system 'Arei)
-        ;; (arei-eldoc-setup)
+        (add-hook 'eldoc-documentation-functions #'arei-eldoc-arglist nil t)
         (add-hook 'completion-at-point-functions #'arei-complete-at-point nil t)
         (add-hook 'xref-backend-functions #'arei--xref-backend nil t)
         ;; (setq next-error-function #'arei-jump-to-compilation-error)
@@ -694,7 +810,8 @@ variable to nil to disable the mode line entirely.")
     ;; Mode cleanup
     ;; (mapc #'kill-local-variable '(next-error-function))
     (remove-hook 'completion-at-point-functions #'arei-complete-at-point t)
-    (remove-hook 'xref-backend-functions #'arei--xref-backend t)))
+    (remove-hook 'xref-backend-functions #'arei--xref-backend t)
+    (remove-hook 'eldoc-documentation-functions #'arei-eldoc-arglist t)))
 
 ;;;###autoload
 (defun arei-mode--maybe-activate ()
